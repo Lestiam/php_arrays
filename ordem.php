0020e497569b39da66c34e8a947df7112a98f060
Igor Teles Caetano <igor.caetano@123milhas.com.br>
2023-05-23 17:55:06 -0300

    estuda sort

---
+++ ordem.php
@@ -0,0 +1,27 @@
+<?php
+
+$notas = [
+    [
+        'aluno' => 'Maria',
+        'nota' => 10,
+        'aluno' => 'Igor',
+        'nota' => 6,
+        'aluno' => 'Ana',
+        'nota' => 9,
+    ],
+    10,
+    8,
+    9,
+    7
+];
+$notasOrdenadas = $notas;
+sort($notasOrdenadas); //função que ordena meu array (atenção ao &, ele significa que vai pegar op array original e ordená-lo), para evitar isso, eu copiei o arrey notas para a variavel $notas ordenadas
+//com numeros, o sort ordenada do menor para o maior, com strings, ele ordena em ordem alfabética
+echo 'Desordenadas: ';
+var_dump($notas); //funcao que exibe uma variavel contendo seu tipo, sua estrutura, etc. é algo que utilizamos para visualizar um dadoecho 'Desordenadas: ';
+
+echo 'Ordenadas: ';
+var_dump($notasOrdenadas); //funcao que exibe uma variavel contendo seu tipo, sua estrutura, etc. é algo que utilizamos para visualizar um dado
+
+
+?>
